77f5256bd37d148ccc473521e33dd385e867aa2f
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2024-11-03 16:28:40 -0300

    change transactions name to correct name

--- src/domain/entities/transactions.ts
+++ src/domain/entities/transactions.ts
@@ -1,13 +1,13 @@
 import { TransactionTypeEnum } from '../enum/transaction-type.enum';
 
-export class Transactions {
+export class Transaction {
   id: string;
   type: TransactionTypeEnum;
   origin: string | null;
   destination: string | null;
   amount: number;
 
-  constructor(data: Transactions) {
+  constructor(data: Transaction) {
     this.id = data.id;
     this.type = data.type;
     this.origin = data.origin;
